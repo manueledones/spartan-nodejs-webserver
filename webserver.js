dbdb4688c816928b37c9a3f1d44b22f7cece8a4f
manuele dones <manueledones@gmail.com>
2016-12-08 11:14:04 +0100

    Added gitignore

--- webserver.js
+++ webserver.js
@@ -4,6 +4,6 @@ var app = express();
 var port = 3000;
 var staticPath = path.join(__dirname, '../typescript-knockout-scaffold/app');
 app.use('/', express.static(staticPath));
-var listener = app.listen(port, function() {
-    console.log('listening on port ' + listener.address().port + 'from folder ' + staticPath);
+var listener = app.listen(port, 'localhost', function() {
+    console.log('Providing path' + staticPath + ' on http://' + listener.address().address + ':' + listener.address().port);
 })
